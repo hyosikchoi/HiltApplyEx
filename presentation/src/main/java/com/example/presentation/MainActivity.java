24599c9394b40b02f14c7aed1f2bf5431c8882af
hyosikchoi <huchoi24@naver.com>
2023-11-14 00:05:21 +0900

    Add - Multimodule of presentation

---
+++ presentation/src/main/java/com/example/presentation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.presentation;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
